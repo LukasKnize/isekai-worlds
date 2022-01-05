8908fba16613314502a729441430b03cd382c300
LukasKnize <82168954+LukasKnize@users.noreply.github.com>
2022-01-05 22:09:54 +0100

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,15 @@
+function mainMenu() {
+    document.getElementById("mainMenu").classList.toggle("show");
+}
+
+function shop() {
+    document.getElementById("shop").classList.toggle("show");
+}
+
+function quests() {
+    document.getElementById("quests").classList.toggle("show");
+}
+
+function actions() {
+    document.getElementById("actions").classList.toggle("show");
+}
