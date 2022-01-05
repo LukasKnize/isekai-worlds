173905ae656ff0e25fd89c31cf679e0c44b0f6b2
LukasKnize <82168954+LukasKnize@users.noreply.github.com>
2022-01-05 22:25:11 +0100

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,5 +1,12 @@
 function mainMenu() {
     document.getElementById("mainMenu").classList.toggle("show");
+    if(document.getElementById("credits").classList.contains("show")){
+        document.getElementById("credits").classList.toggle("show");
+    }
+
+    if(document.getElementById("about").classList.contains("show")){
+        document.getElementById("about").classList.toggle("show");
+    }
 }
 
 function shop() {
@@ -13,3 +20,12 @@ function quests() {
 function actions() {
     document.getElementById("actions").classList.toggle("show");
 }
+
+function credits() {
+    document.getElementById("credits").classList.toggle("show");
+}
+
+function about() {
+    document.getElementById("about").classList.toggle("show");
+}
+
